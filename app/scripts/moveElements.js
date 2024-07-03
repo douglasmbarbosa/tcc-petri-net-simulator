048f823405b90b3e8426d4d95dffbc58d49547c3
douglasmbarbosa <douglasbarbosa081@gmail.com>
2024-07-03 17:07:38 -0300

    Movimentando arcos pelas bordas dos elementos

--- app/scripts/moveElements.js
+++ app/scripts/moveElements.js
@@ -1,6 +1,6 @@
 // movendo o arco \
 
-function adjustedPositionArc(arc, mouseX, mouseY, posCenterX, posCenterY) {
+function adjustedPositionArc(arc, mouseX, mouseY, posCenterX, posCenterY, type) {
 
     var point = arc.substring(0, arc.indexOf(' '))
     var arcName = `${arc.substring(arc.indexOf(' ') + 1)}`
@@ -11,14 +11,21 @@ function adjustedPositionArc(arc, mouseX, mouseY, posCenterX, posCenterY) {
         var arcPosFinalX = arrayArcs[idArc].endPositionArc[0][0]
         var arcPosFinalY = arrayArcs[idArc].endPositionArc[0][1]
 
-        var distance = Math.sqrt(Math.pow(arcPosFinalX - posCenterX,2) + Math.pow(arcPosFinalY - posCenterY,2)) 
+        if (type == "place") {
+            var distance = Math.sqrt(Math.pow(arcPosFinalX - posCenterX,2) + Math.pow(arcPosFinalY - posCenterY,2)) 
+            var shortestDistanceX = posCenterX + ((arcPosFinalX - posCenterX)/distance) * radius
+            var shortestDistanceY = posCenterY + ((arcPosFinalY - posCenterY)/distance) * radius 
+        }
 
-        var shortestDistanceX = posCenterX + ((arcPosFinalX - posCenterX)/distance) * radius
-        var shortestDistanceY = posCenterY + ((arcPosFinalY - posCenterY)/distance) * radius
-
+        else if (type == "transition") {
+            var shortestDistanceX =  Math.max(posCenterX, Math.min(arcPosFinalX, posCenterX + transitionWidth))
+            var shortestDistanceY =  Math.max(posCenterY, Math.min(arcPosFinalY, posCenterY + transitionHeigth))
+        }
 
         arrayArcs[idArc].startingPositionArc[0][0] = shortestDistanceX  
-        arrayArcs[idArc].startingPositionArc[0][1] = shortestDistanceY  
+        arrayArcs[idArc].startingPositionArc[0][1] = shortestDistanceY 
+
+        
 
     }
 
@@ -27,10 +34,17 @@ function adjustedPositionArc(arc, mouseX, mouseY, posCenterX, posCenterY) {
         var arcPosFinalX = arrayArcs[idArc].startingPositionArc[0][0]
         var arcPosFinalY = arrayArcs[idArc].startingPositionArc[0][1]
 
-        var distance = Math.sqrt(Math.pow(arcPosFinalX - posCenterX,2) + Math.pow(arcPosFinalY - posCenterY,2)) 
+        if (type == "place") {
+            var distance = Math.sqrt(Math.pow(arcPosFinalX - posCenterX,2) + Math.pow(arcPosFinalY - posCenterY,2)) 
 
-        var shortestDistanceX = posCenterX + ((arcPosFinalX - posCenterX)/distance) * radius
-        var shortestDistanceY = posCenterY + ((arcPosFinalY - posCenterY)/distance) * radius
+            var shortestDistanceX = posCenterX + ((arcPosFinalX - posCenterX)/distance) * radius
+            var shortestDistanceY = posCenterY + ((arcPosFinalY - posCenterY)/distance) * radius
+        }
+
+        else if (type == "transition") {
+            var shortestDistanceX =  Math.max(posCenterX, Math.min(arcPosFinalX, posCenterX + transitionWidth))
+            var shortestDistanceY =  Math.max(posCenterY, Math.min(arcPosFinalY, posCenterY + transitionHeigth))
+        }
 
         arrayArcs[idArc].endPositionArc[0][0] = shortestDistanceX
         arrayArcs[idArc].endPositionArc[0][1] = shortestDistanceY
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,31 +1,5 @@
-var canvas = document.getElementById('petri-net-canvas');
-var ctx = canvas.getContext('2d');
-var buttonPress = 0; 
-var arrayPlaces = []; 
-var arrayTransitions = [];
-var arrayArcs = [];
-var arcs = [];
-var startingPositionArc= []
-var endPositionArc = []
-var intermediatePoints = []
-var nPlaces = 0;
-var nTransitions = 0;
-var nArcs = 0;
-var nTokens = 0;
-var isMoving = false;
-var isEnabled = false;
-var isPress = false
-var drawArc = false
-var selectFirtsPoint = false
-var isInsidePlace = false
-var isInsideTransition = false
-var typeElement = null
-const radius = 25;
-const transitionHeigth = 50
-const transitionWidth = 20
-
-function loop(){
-    window.requestAnimationFrame(loop,canvas);
+function loop() {
+    window.requestAnimationFrame(loop, canvas);
     render();
 }
 
@@ -33,7 +7,7 @@ canvas.addEventListener('mousedown', (event) => {
     //Obtendo posição do ponteiro do mouse
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top; 
+    const mouseY = event.clientY - rect.top;
     if (buttonPress == 1) {
         objPlace = {
             id: nPlaces,
@@ -51,21 +25,21 @@ canvas.addEventListener('mousedown', (event) => {
         objTransition = {
             id: nTransitions,
             Name: `transition ${nTransitions + 1}`,
-            posX: mouseX - transitionWidth/2,
-            posY: mouseY - transitionHeigth/2,
+            posX: mouseX - transitionWidth / 2,
+            posY: mouseY - transitionHeigth / 2,
             connections: [],
             isEnabled: isEnabled
         }
-        arrayTransitions.push(objTransition);      
+        arrayTransitions.push(objTransition);
         nTransitions += 1;
-    }   
-    
-    if (buttonPress == 3 ){
-        
-        drawArc= true
-        
-        for (var place of arrayPlaces){
-            isInsidePlace = insidePlace(mouseX, mouseY, place.posX,place.posY)
+    }
+
+    if (buttonPress == 3) {
+
+        drawArc = true
+
+        for (var place of arrayPlaces) {
+            isInsidePlace = insidePlace(mouseX, mouseY, place.posX, place.posY)
 
             const dx = mouseX - place.posX;
             const dy = mouseY - place.posY;
@@ -73,28 +47,29 @@ canvas.addEventListener('mousedown', (event) => {
             const adjustedX = place.posX + (dx / distance) * radius;
             const adjustedY = place.posY + (dy / distance) * radius;
 
-            posEdge = [adjustedX,adjustedY, place.Name]
+            posEdge = [adjustedX, adjustedY, place.Name]
 
-            if(isInsidePlace && startingPositionArc.length == 0) {
-                
+            if (isInsidePlace && startingPositionArc.length == 0) {
+
                 startingPositionArc.push(posEdge)
                 place.connections.push(`Start Arc ${nArcs + 1}`)
                 typeElement = "place"
             }
-            else if(isInsidePlace && typeElement == "transition"){
+            else if (isInsidePlace && typeElement == "transition") {
                 endPositionArc.push(posEdge)
                 place.connections.push(`Finish Arc ${nArcs + 1}`)
                 drawArc = false
                 typeElement = null
-            }   
+            }
+
         }
-        
-        for (var transition of arrayTransitions){
-            isInsideTransition = insideTransition(mouseX, mouseY, transition.posX,transition.posY)
 
-            mouseXY = [mouseX,mouseY,transition.Name]
+        for (var transition of arrayTransitions) {
+            isInsideTransition = insideTransition(mouseX, mouseY, transition.posX, transition.posY)
 
-            if(isInsideTransition && startingPositionArc.length == 0) {
+            mouseXY = [mouseX, mouseY, transition.Name]
+
+            if (isInsideTransition && startingPositionArc.length == 0) {
                 startingPositionArc.push(mouseXY)
                 transition.connections.push(`Start Arc ${nArcs + 1}`)
                 typeElement = "transition"
@@ -110,68 +85,62 @@ canvas.addEventListener('mousedown', (event) => {
             objArc = {
                 id: nTransitions,
                 Name: `Arc ${nArcs + 1}`,
-                startingPositionArc: startingPositionArc, 
+                startingPositionArc: startingPositionArc,
                 endPositionArc: endPositionArc,
                 intermediatePoints: intermediatePoints,
                 weight: 1
-            } 
+            }
             arrayArcs.push(objArc);
-            nArcs += 1; 
+            nArcs += 1;
             startingPositionArc = [];
             endPositionArc = [];
-            
-        }        
+
+        }
     }
-    
-    if (drawArc == false){
-        buttonPress = 0 
+
+    if (drawArc == false) {
+        buttonPress = 0
     }
-      
+
     //Verifica se o botão pressionado foi o esquerdo  
     if (event.button == 0) {
         isPress = true
-    }    
+    }
 })
 
 canvas.addEventListener('mousemove', (event) => {
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;  
-     for (var place of arrayPlaces){
-        isInsidePlace = insidePlace(mouseX, mouseY, place.posX,place.posY)
-        
+    const mouseY = event.clientY - rect.top;
+    for (var place of arrayPlaces) {
+        isInsidePlace = insidePlace(mouseX, mouseY, place.posX, place.posY)
+
         canvas.style.cursor = isPress ? 'grabbing' : 'default'
-        
+
         if (isInsidePlace && isPress) {
             place.posX = mouseX;
             place.posY = mouseY;
 
-            for (var arc of place.connections){
-                adjustedPositionArc(arc, mouseX, mouseY,place.posX,place.posY)
+            for (var arc of place.connections) {
+                adjustedPositionArc(arc, mouseX, mouseY, place.posX, place.posY, "place")
             }
-
-            
-
-
-
-            //console.log(place.Name)
         }
-    } 
-    for (var transition of arrayTransitions){
-        isInsideTransition = insideTransition(mouseX, mouseY, transition.posX,transition.posY)
+    }
+    for (var transition of arrayTransitions) {
+        isInsideTransition = insideTransition(mouseX, mouseY, transition.posX, transition.posY)
         canvas.style.cursor = isPress ? 'grabbing' : 'default'
         if (isInsideTransition && isPress) {
-            transition.posX = mouseX - transitionWidth/2;
-            transition.posY = mouseY - transitionHeigth/2;
+            transition.posX = mouseX - transitionWidth / 2;
+            transition.posY = mouseY - transitionHeigth / 2;
 
-            for (var arc of transition.connections){
-                adjustedPositionArc(arc, mouseX, mouseY,transition.posX,transition.posY)
+            for (var arc of transition.connections) {
+                adjustedPositionArc(arc, mouseX, mouseY, transition.posX, transition.posY, "transition")
             }
 
             //console.log(transition.Name)
-        }   
+        }
     }
-    
+
 })
 
 canvas.addEventListener('mouseup', (event) => {
