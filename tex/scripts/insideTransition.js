3cce110727f698f7d33ecbdeee19bb2c644e5fcc
douglasmbarbosa <douglasbarbosa081@gmail.com>
2025-05-29 05:56:26 -0700

    Escrita da parte de desenvolvimento 21

--- tex/scripts/insideTransition.js
+++ tex/scripts/insideTransition.js
@@ -2,3 +2,11 @@ function insideTransition(mouseX, mouseY, transitionX, transitionY) {
     var a = (mouseX >= transitionX) && (mouseX <= transitionX + transitionWidth) && (mouseY >= transitionY) && (mouseY <= transitionY + transitionHeigth)
     return a;
 }
+
+ for (var transition of arrayTransitions) {
+    isInsideTransition = insideTransition(mouseX, mouseY, transition.posX, transition.posY)    
+    if (isPress && isInsideTransition) {
+        transition.posX = mouseX - transitionWidth / 2;
+        transition.posY = mouseY - transitionHeigth / 2;
+    }
+}
